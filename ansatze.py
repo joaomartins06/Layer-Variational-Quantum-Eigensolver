01d707e1b027f81c689b53d1fe5919acbfb420c1
joaomartins06 <joaoferreirama@tudelft.nl>
2026-05-14 18:42:39 +0200

    Add CommunityDetection, Ansatz classes and requirements

---
+++ ansatze.py
@@ -0,0 +1,79 @@
+import numpy as np
+
+
+class Ansatz:
+    ''' 
+    Define the ansatz ciruit to be optimsed. 
+    This class creates an abstraction from how we compute things
+    For smaller graphs and for a noisy case, we will be using a circuit.
+    For bigger graphs, we will be using a tensor network (there is a limit of around 25 qubits for the circuit)
+    '''
+
+    def __init__(self, n_qubits: int):
+        self.n_qubits = n_qubits
+        self.n_layers = 0
+
+        #pre compute the CNOT pairs for the layers > 0
+        self.even_odd_pairs = [(i, i+1) for i in range(0, n_qubits-1, 2)]
+        self.odd_even_pairs = [(i, i+1) for i in range(1, n_qubits-1, 2)]
+
+        #randomly initialise parameters for the first layer
+        self.params = np.random.uniform(0, 2*np.pi, size=n_qubits)
+
+
+    def add_layer(self):
+        #easy to check that, accoriding to the paper's construction, each layer adds 4*n_qubits - 4 rotations
+        n_new = 4 * self.n_qubits - 4  
+        self.params = np.concatenate([self.params, np.zeros(n_new)])
+        self.n_layers += 1
+
+
+    def param_count(self):
+        return len(self.params)
+
+
+    def get_gates(self, params: np.ndarray) -> list:
+        #get the list of gates
+        #for the rotations, and element is ('Ry', qubit_index, rotation_angle)
+        #for the CNOTs, an element is ('CNOT', control_qubit_index, target_qubit_index)
+        #this will be used later
+
+        gates = []
+        idx = 0 
+
+        #ayer 0
+        for q in range(self.n_qubits):
+            gates.append(('Ry', q, params[idx]))
+            idx += 1
+
+        #layer > 0
+        for _ in range(self.n_layers):
+            
+            #start with a series of CNOTs then Ry on all qubits
+            #this is the even-odd part
+            for control, target in self.even_odd_pairs:
+                gates.append(('CNOT', control, target))
+            for q in range(self.n_qubits):
+                gates.append(('Ry', q, params[idx]))
+                idx += 1
+            #repeat the even-odd part
+            for control, target in self.even_odd_pairs:
+                gates.append(('CNOT', control, target))
+            for q in range(self.n_qubits):
+                gates.append(('Ry', q, params[idx]))
+                idx += 1
+
+            #same thing, but now for the odd-even part
+            for control, target in self.odd_even_pairs:
+                gates.append(('CNOT', control, target))
+            for q in self.odd_even_qubits:
+                gates.append(('Ry', q, params[idx]))
+                idx += 1
+            #repeat the odd-even part
+            for control, target in self.odd_even_pairs:
+                gates.append(('CNOT', control, target))
+            for q in self.odd_even_qubits:
+                gates.append(('Ry', q, params[idx]))
+                idx += 1
+
+        return gates
---
+++ community_detection.py
@@ -0,0 +1,124 @@
+import numpy as np
+import networkx as nx
+from itertools import product as iterproduct
+
+
+
+class CommunityDetection:
+    """
+     Defines the community detection problem and has some methods to compute 
+     the Hamiltonian and best known value.
+    """
+
+    def __init__(self, graph: nx.Graph, k: int):
+        self.graph = graph
+        self.k = k
+        self.n_nodes = graph.number_of_nodes()
+        self.m = graph.number_of_edges()
+        #number of bits/qubits to encode k communities
+        self.N = int(np.ceil(np.log2(k)))
+        self.n_qubits = self.n_nodes * self.N
+
+        # modularity matrix B_{u,v} = A_{u,v} - d_u*d_v / (2m)
+        #taken from the paper
+
+        #get adjacency matrix and degree vector
+        A = nx.to_numpy_array(graph)
+        #get the degree of each node
+        degrees = np.array([d for _, d in graph.degree()])
+        #compute the modularity matrix B
+        self.B = A - np.outer(degrees, degrees) / (2 * self.m)
+
+        # build Hamiltonian once and cache it
+        self.terms = self._build_hamiltonian()
+
+
+    def _qubit_index(self, bit: int, node: int):
+        return bit * self.n_nodes + node
+
+
+    def _build_hamiltonian(self):
+ 
+        terms = []
+        #denominator in the hamiltonian, takes into account the 1/2 in the product
+        prefactor = -1.0 / (2 * self.m * (2 ** self.N))
+
+        for u in range(self.n_nodes):
+            for v in range(self.n_nodes):
+                b_uv = self.B[u, v]
+                # skip negligible terms in case they are not really 0
+                if abs(b_uv) < 1e-12:
+                    continue
+                
+                coeff_base = prefactor * b_uv
+                #gets all combinations of bits for the communities depending on the encoding 
+                #this is the loop from j= 1 to N in the paper
+                for j in iterproduct([0, 1], repeat=self.N):
+
+                    #qubits where Z acts in this particular term
+                    qubits = []
+
+                    for s, bit in enumerate(j):
+                        #bit=0 means this position contributes ident
+                        #bit=1 means this position contributes ZZ
+                        if bit:
+                            qubits.append(self._qubit_index(s, u))
+                            qubits.append(self._qubit_index(s, v))
+
+                    terms.append((coeff_base, sorted(qubits)))
+
+        return terms
+    
+
+    def _modularity(self, assignment):
+        Q = 0.0
+        for u in range(self.n_nodes):
+            for v in range(self.n_nodes):
+                if assignment[u] == assignment[v]:
+                    Q += self.B[u, v]
+        return Q / (2 * self.m)
+
+
+    def best_known_value(self):
+
+        if self.n_nodes <= 12:
+            return self._brute_force_optimum()
+        else:
+            return self._louvain_optimum()
+
+
+    def _brute_force_optimum(self):
+        #just brute force all possible assignments of nodes to communities and compute the modularity
+        best = -np.inf
+        for assignment in iterproduct(range(self.k), repeat=self.n_nodes):
+            Q = self._modularity(assignment)
+            if Q > best:
+                best = Q
+        return best
+
+
+    def _louvain_optimum(self):
+        #gets the best modularity found by the louvain algorithm
+        from networkx.algorithms.community import louvain_communities
+        best = -np.inf
+        for _ in range(20):
+            communities = louvain_communities(self.graph, seed=None)
+            # convert to assignment list
+            assignment = [0] * self.n_nodes
+            nodes = list(self.graph.nodes())
+            for comm_idx, comm in enumerate(communities):
+                for node in comm:
+                    assignment[nodes.index(node)] = comm_idx
+            Q = self._modularity(tuple(assignment))
+            if Q > best:
+                best = Q
+
+        return best
+
+
+    def set_best_known_value(self, value: float):
+        self._best_known_value = value
+
+
+    def approximation_ratio(self, energy: float, best_known: float) -> float:
+        return energy / best_known
